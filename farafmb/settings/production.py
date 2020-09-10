0ea56164fc3e1f42b773399c89c37db68f52b8aa
aiventimptner <aiven.timptner@icloud.com>
2020-09-10 14:50:00 +0200

    Set deploymant settings for production

--- farafmb/settings/production.py
+++ farafmb/settings/production.py
@@ -2,6 +2,14 @@ import os
 
 from .base import *
 
+# Debug
 DEBUG = False
 
+# Allowed hosts
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+
+# Security
+SECURE_HSTS_SECONDS = 3600  # TODO set value to 31536000 when tested properly
+SECURE_SSL_REDIRECT = False  # nginx already redirects to https
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
