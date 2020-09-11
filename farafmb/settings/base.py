16e909e0fae0f3efb1cbe59785d34baaaa833bd5
Aiven Timptner <aiven.timptner@icloud.com>
2020-09-11 15:47:49 +0200

    Test git hook

--- farafmb/settings/base.py
+++ farafmb/settings/base.py
@@ -116,4 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'farafmb' / 'static',
+# ]
 STATIC_ROOT = BASE_DIR / 'static'
