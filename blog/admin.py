70b25336d18f29c887e756eb9840421860c3ea9b
Aiven Timptner <aiven.timptner@icloud.com>
2020-10-30 19:19:41 +0100

    Rework admin pages

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,8 @@ from .models import Snippet, Post, Image
 
 @admin.register(Snippet)
 class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('desc', 'slug', 'created')
+
     def save_model(self, request, obj, form, change):
         if re.search(r'!\[.+]\(.+\)', obj.content):
             messages.warning(request, "Einbetten von Bildern per Hyperlink wird blockiert. "
@@ -17,7 +19,12 @@ class SnippetAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('author',)
+    list_display = ('title', 'image', 'author', 'created')
+    list_filter = ('author',)
+
     def save_model(self, request, obj, form, change):
+        obj.author = request.user
         if re.search(r'!\[.+]\(.+\)', obj.content):
             messages.warning(request, "Einbetten von Bildern per Hyperlink wird blockiert. "
                                       "Bitte 'Blog->Images' verwenden.")
@@ -27,4 +34,4 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug', 'created')
