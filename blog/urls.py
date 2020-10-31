be44b89ea6ed17a688ece7cb77a21ae4cd5bf218
Aiven Timptner <aiven.timptner@icloud.com>
2020-10-31 02:57:46 +0100

    Add archive

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,15 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('blog/', views.PostsView.as_view(), name='posts'),
     path('sprechzeiten/', views.OfficeHoursView.as_view(), name='office_hours'),
+
+    path('archiv/',
+         views.PostArchiveIndexView.as_view(), name='post_archive'),
+    path('archiv/<int:year>/',
+         views.PostYearArchiveView.as_view(), name='post_year_archive'),
+    path('archiv/<int:year>/<int:month>/',
+         views.PostMonthArchiveView.as_view(month_format='%m'), name='post_month_archive'),
+    path('archiv/<int:year>/<int:month>/<int:day>/',
+         views.PostDayArchiveView.as_view(month_format='%m'), name='post_day_archive'),
 ]
 
 if settings.DEBUG:
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.template.defaultfilters import date
 from django.views import generic
 
 from .models import Snippet, Post, Image
@@ -20,6 +21,15 @@ class PostsView(generic.ListView):
     template_name = 'blog/posts.html'
     ordering = ['-created']
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['archive'] = []
+        year_list = Post.objects.dates('created', 'year')
+        for dt in year_list:
+            month_list = Post.objects.filter(created__year=dt.year).dates('created', 'month')
+            context['archive'].append((dt, month_list))
+        return context
+
 
 class OfficeHoursView(generic.TemplateView):
     template_name = 'blog/office_hours.html'
@@ -28,3 +38,24 @@ class OfficeHoursView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['snippet'] = Snippet.objects.filter(slug='office_hours').first()
         return context
+
+
+class PostArchiveIndexView(generic.dates.ArchiveIndexView):
+    model = Post
+    date_field = 'created'
+
+
+class PostYearArchiveView(generic.dates.YearArchiveView):
+    model = Post
+    date_field = 'created'
+    make_object_list = True
+
+
+class PostMonthArchiveView(generic.dates.MonthArchiveView):
+    model = Post
+    date_field = 'created'
+
+
+class PostDayArchiveView(generic.dates.DayArchiveView):
+    model = Post
+    date_field = 'created'
