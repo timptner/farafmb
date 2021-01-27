4f31fc24e94a771ea6feb440bf07680711e2793b
Aiven Timptner <aiven.timptner@icloud.com>
2021-01-27 02:26:51 +0100

    Add oidc login for keycloak

--- farafmb/settings.py
+++ farafmb/settings.py
@@ -17,9 +17,9 @@ ALLOWED_HOSTS = ['dev.farafmb.de', 'farafmb.de', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-
     'django.contrib.admin',
     'django.contrib.auth',
+    'mozilla_django_oidc',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'mozilla_django_oidc.middleware.SessionRefresh',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -72,6 +73,31 @@ DATABASES = {
     }
 }
 
+
+# Authentication
+
+AUTHENTICATION_BACKENDS = [
+    'farafmb.auth.KeycloakBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+OIDC_RP_CLIENT_ID = os.getenv('OIDC_CLIENT_ID')
+
+OIDC_RP_CLIENT_SECRET = os.getenv('OIDC_CLIENT_SECRET')
+
+OIDC_RP_SIGN_ALGO = "RS256"
+
+OIDC_OP_JWKS_ENDPOINT = "https://auth.faking.cool/auth/realms/faking/protocol/openid-connect/certs"
+
+OIDC_OP_AUTHORIZATION_ENDPOINT = "https://auth.faking.cool/auth/realms/faking/protocol/openid-connect/auth"
+
+OIDC_OP_TOKEN_ENDPOINT = "https://auth.faking.cool/auth/realms/faking/protocol/openid-connect/token"
+
+OIDC_OP_USER_ENDPOINT = "https://auth.faking.cool/auth/realms/faking/protocol/openid-connect/userinfo"
+
+LOGIN_REDIRECT_URL = "/admin/"
+
+LOGOUT_REDIRECT_URL = "/"
 
 # Password validation
 
--- farafmb/urls.py
+++ farafmb/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('oidc/', include('mozilla_django_oidc.urls')),
 ]
