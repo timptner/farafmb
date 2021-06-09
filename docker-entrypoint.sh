62da891f2b555356a60d8c11b0bc40e5c22a1580
Aiven <aiven@timptner.de>
2021-06-09 19:48:45 +0200

    Add docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z postgres 5432; do
+  sleep 0.1
+done
+
+echo "Postgres started"
+
+python manage.py flush --no-input
+
+python manage.py migrate
+python manage.py collectstatic --no-input --clear
+
+exec "$@"
