e5cfa0a04568be8a57c6f222230e531f1b9aac16
Aiven <aiven@timptner.de>
2021-06-15 21:11:32 +0200

    Move file serving for development to main root url_conf

--- farafmb/urls.py
+++ farafmb/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('oidc/', include('mozilla_django_oidc.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
