581bbd8f7e85c51a3e2223d5a4ea78b7b24069da
Aiven <aiven@timptner.de>
2021-06-25 00:54:22 +0200

    Add admin actions

--- office_hours/admin.py
+++ office_hours/admin.py
@@ -1,8 +1,28 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 
 from .models import OfficeHour
 
 
 @admin.register(OfficeHour)
 class OfficeHourAdmin(admin.ModelAdmin):
-    pass
+
+    actions = ['make_visible', 'make_invisible']
+
+    @admin.action(description='Mark selected office hours as visible')
+    def make_visible(self, request, queryset):
+        updated = queryset.update(is_visible=True)
+        self.message_user(request, ngettext(
+            '%d office hours was successfully marked as visible.',
+            '%d office hours were successfully marked as visible.',
+            updated,
+        ) % updated, messages.SUCCESS)
+
+    @admin.action(description='Mark selected office hours as invisible')
+    def make_invisible(self, request, queryset):
+        updated = queryset.update(is_visible=False)
+        self.message_user(request, ngettext(
+            '%d office hours was successfully marked as invisible.',
+            '%d office hours were successfully marked as invisible.',
+            updated,
+        ) % updated, messages.SUCCESS)
