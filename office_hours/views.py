e0ce7224bf70a71793553f55c113e1b77bd2fcec
Aiven <aiven@timptner.de>
2021-06-25 00:59:15 +0200

    Only calculate table if data is available to display

--- office_hours/views.py
+++ office_hours/views.py
@@ -10,23 +10,24 @@ class OfficeHoursView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        office_hours = OfficeHour.objects.filter(is_visible=True).all()
-        time_list = [office_hour.time for office_hour in office_hours]
-        seconds = calc_max_step_size(time_list)
-        points = range(
-            time_to_seconds(min(time_list)),
-            time_to_seconds(max(time_list)) + seconds + seconds,
-            seconds,
-        )
 
         context['data'] = {}
-        for time_ in [seconds_to_time(seconds) for seconds in points]:
-            slots = []
-            for day, name in OfficeHour.DAYS:
-                if time_ in time_list:
-                    slots.append(office_hours.filter(time=time_, day=day).all())
-                else:
-                    slots.append([])
-            context['data'][time_] = slots
+        office_hours = OfficeHour.objects.filter(is_visible=True).all()
+        if office_hours:
+            time_list = [office_hour.time for office_hour in office_hours]
+            seconds = calc_max_step_size(time_list)
+            points = range(
+                time_to_seconds(min(time_list)),
+                time_to_seconds(max(time_list)) + seconds + seconds,
+                seconds,
+            )
+            for time_ in [seconds_to_time(seconds) for seconds in points]:
+                slots = []
+                for day, name in OfficeHour.DAYS:
+                    if time_ in time_list:
+                        slots.append(office_hours.filter(time=time_, day=day).all())
+                    else:
+                        slots.append([])
+                context['data'][time_] = slots
         context['days'] = OfficeHour.DAYS
         return context
