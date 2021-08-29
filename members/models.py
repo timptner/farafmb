0ec48dac1a6d883d75350d38d58911e14082cca2
Aiven <aiven@timptner.de>
2021-08-29 22:08:55 +0200

    Update profile model

--- members/models.py
+++ members/models.py
@@ -3,11 +3,29 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    EMO = 'EMO'
+    IDE = 'IDE'
+    MB = 'MB'
+    MTK = 'MTK'
+    SEM = 'SEM'
+    WLO = 'WLO'
+    WMB = 'WMB'
+    COURSES = [
+        (EMO, 'Elektromobilität'),
+        (IDE, 'Integrated Design Engineering'),
+        (MB, 'Maschinenbau'),
+        (MTK, 'Mechatronik'),
+        (SEM, 'Systems Engineering for Manufacturing'),
+        (WLO, 'Wirtschaftsingenieur Logistik'),
+        (WMB, 'Wirtschaftsingenieur Maschinenbau'),
+    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     picture = models.ImageField('members')
-    biography = models.CharField(max_length=250)
-    jobs = models.CharField(max_length=100)
-    course = models.CharField(max_length=50)
-    semester = models.PositiveIntegerField()
-    birthday = models.DateTimeField(blank=True, null=True)
-    joined_at = models.DateTimeField()
+    biography = models.CharField(max_length=250, blank=True)
+    jobs = models.CharField(max_length=100, blank=True)
+    course = models.CharField(max_length=3, choices=COURSES)
+    birthday = models.DateField(blank=True, null=True)
+    joined_at = models.DateField()
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
