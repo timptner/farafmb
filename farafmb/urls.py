57443b6fcc5feab75505f42b47c323cafe95e72c
Aiven <aiven@timptner.de>
2021-08-31 01:01:13 +0200

    Remove whitenoise

--- farafmb/urls.py
+++ farafmb/urls.py
@@ -11,7 +11,3 @@ urlpatterns = [
     path('members/', include('members.urls')),
     path('office-hours/', include('office_hours.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
