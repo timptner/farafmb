9a9d4697f1d18ab2026bea9f2c0e6ee0c4a243dd
Aiven <aiven@timptner.de>
2021-09-01 00:54:04 +0200

    Update path name

--- members/urls.py
+++ members/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'members'
 urlpatterns = [
-    path('', views.MemberListView.as_view(), name='list'),
+    path('', views.MemberListView.as_view(), name='member_list'),
 ]
