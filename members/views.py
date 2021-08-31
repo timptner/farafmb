396d05145cb9d7e9ce1be0ac89a50e47707cbbee
Aiven <aiven@timptner.de>
2021-08-31 23:35:14 +0200

    Only list users with profile

--- members/views.py
+++ members/views.py
@@ -1,7 +1,13 @@
 from django.views import generic
 from django.contrib.auth.models import User
 
+from .models import Profile
+
 
 class MemberList(generic.ListView):
     model = User
     template_name = 'members/list.html'
+
+    def get_queryset(self):
+        user_list = Profile.objects.values_list('user', flat=True).all()
+        return User.objects.filter(pk__in=user_list).all()
