ad870e1be736a2f95acdecd1458361e534e59c2b
Aiven <aiven@timptner.de>
2021-09-01 00:44:53 +0200

    Serve media files in development

--- farafmb/urls.py
+++ farafmb/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('members/', include('members.urls')),
     path('office-hours/', include('office_hours.urls')),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
