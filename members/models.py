cb6ea5f6d181ef92c8881b2caf6d0177a8e9c4d0
Aiven <aiven@timptner.de>
2021-09-07 00:15:11 +0200

    Add degree to profile

--- members/models.py
+++ members/models.py
@@ -1,6 +1,6 @@
 from datetime import date
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 from farafmb.helpers import get_random_filename_and_upload_to
 
@@ -22,11 +22,18 @@ class Profile(models.Model):
         (WLO, 'Wirtschaftsingenieur Logistik'),
         (WMB, 'Wirtschaftsingenieur Maschinenbau'),
     ]
+    BSC = 'BSC'
+    MSC = 'MSC'
+    DEGREES = [
+        (BSC, 'Bachelor of Science'),
+        (MSC, 'Master of Science'),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to=get_random_filename_and_upload_to('members'))
     biography = models.CharField(max_length=250, blank=True)
     jobs = models.CharField(max_length=100, blank=True)
     course = models.CharField(max_length=3, choices=COURSES)
+    degree = models.CharField(max_length=3, choices=DEGREES)
     birthday = models.DateField(blank=True, null=True)
     joined_at = models.DateField()
 
