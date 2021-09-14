2c771be597f46c7f3d41766295be9fa62cac0f1a
Aiven <aiven@timptner.de>
2021-09-15 00:26:32 +0200

    Add excursion detail page

--- excursions/urls.py
+++ excursions/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'excursions'
 urlpatterns = [
     path('', views.ExcursionListView.as_view(), name='list'),
+    path('<int:pk>/', views.ExcursionDetailView.as_view(), name='detail'),
 ]
--- excursions/views.py
+++ excursions/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Excursion
+from .models import Excursion, Participant
 
 
 class ExcursionListView(generic.ListView):
     model = Excursion
+
+
+class ExcursionDetailView(generic.DetailView):
+    model = Excursion
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['registrations'] = Participant.objects.filter(excursion=kwargs['object']).count()
+        return data
