96d038bfb4eee8dd6f28c9156309524280bbafd8
Aiven <aiven@timptner.de>
2021-10-23 12:27:25 +0200

    Add storage for use in views

--- docs/views.py
+++ docs/views.py
@@ -1,2 +1,16 @@
+import os
+
+from django.conf import settings
 from django.shortcuts import render
+from django_s3_storage.storage import S3Storage
+
+storage = S3Storage(
+    aws_region=settings.AWS_REGION,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    aws_s3_bucket_name=os.getenv('DO_SPACE_NAME_DOCS', 'farafmb-docs'),
+    aws_s3_endpoint_url=os.getenv('DO_SPACE_ENDPOINT_URL_DOCS', 'https://fra1.digitaloceanspaces.com'),
+    aws_s3_file_overwrite=True,
+)
+
 
