f2341d6f5fb4a2036ef5da252cf269a4f2008676
Aiven <aiven@timptner.de>
2021-10-23 22:56:19 +0200

    Update urls.py

--- docs/urls.py
+++ docs/urls.py
@@ -4,12 +4,11 @@ from . import views
 
 app_name = 'docs'
 urlpatterns = [
-    path('_new', views.create_page, {'page': '/'}, name='create_page_index'),
-    path('<path:page>/_edit', views.update_page, name='update_page'),
-    path('<path:page>/_delete', views.delete_page, name='delete_page'),
-    path('<path:page>/_new', views.create_page, name='create_page'),
-    path('<path:page>', views.read_page, name='read_page'),
     path('', views.show_tree, {'page': '/'}, name='index'),
+    path('edit/<path:page>', views.update_page, name='update_page'),
+    path('delete/<path:page>', views.delete_page, name='delete_page'),
+    path('new/', views.create_page, name='create_page'),
+    path('read/<path:page>', views.read_page, name='read_page'),
     path('tree/', views.show_tree, {'page': '/'}, name='index_show_tree'),
     path('tree/<path:page>/', views.show_tree, name='show_tree'),
 ]
