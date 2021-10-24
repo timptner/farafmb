1e6cf762c28d329b015f52b1bc54594c01fb4b00
Aiven <aiven@timptner.de>
2021-10-25 00:16:56 +0200

    Support basic syntax

--- farafmb/templatetags/markdown.py
+++ farafmb/templatetags/markdown.py
@@ -1,14 +1,32 @@
+import bleach
+
 from django import template
-from django.utils.html import conditional_escape
+from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 from markdown import markdown as render_markdown
 
 register = template.Library()
 
 
-@register.filter
-def render_html(value):
+@register.filter(needs_autoescape=True)
+@stringfilter
+def render_html(value, autoescape=True):
     """Converts markdown styled text to valid html"""
-    esc = conditional_escape(value)
-    result = render_markdown(esc)
-    return mark_safe(result)
+    # We need to process markdown before sanitizing html
+    # Otherwise some elements (e.g. blockquotes) will break
+    html = render_markdown(value)
+    if autoescape:
+        safe_html = bleach.clean(
+            html,
+            tags=[
+                'a', 'blockquote', 'br', 'code', 'em', 'hr', 'h1', 'h2', 'h3',
+                'h4', 'h5', 'h6', 'img', 'li', 'ol', 'p', 'pre', 'strong', 'ul',
+            ],
+            attributes={
+                'a': ['href', 'title'],
+                'img': ['alt', 'src', 'title'],
+            },
+        )
+    else:
+        safe_html = html
+    return mark_safe(safe_html)
