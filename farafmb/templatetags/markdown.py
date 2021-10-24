5f5c5d31ae2923a65b0a27cee15cdf211adf0d11
Aiven <aiven@timptner.de>
2021-10-25 00:30:55 +0200

    Add extension: table of contents

--- farafmb/templatetags/markdown.py
+++ farafmb/templatetags/markdown.py
@@ -1,9 +1,9 @@
 import bleach
+import markdown
 
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
-from markdown import markdown as render_markdown
 
 register = template.Library()
 
@@ -14,16 +14,23 @@ def render_html(value, autoescape=True):
     """Converts markdown styled text to valid html"""
     # We need to process markdown before sanitizing html
     # Otherwise some elements (e.g. blockquotes) will break
-    html = render_markdown(value)
+    html = markdown.markdown(value, extensions=['toc'])
     if autoescape:
         safe_html = bleach.clean(
             html,
             tags=[
-                'a', 'blockquote', 'br', 'code', 'em', 'hr', 'h1', 'h2', 'h3',
+                'a', 'blockquote', 'br', 'code', 'div', 'em', 'hr', 'h1', 'h2', 'h3',
                 'h4', 'h5', 'h6', 'img', 'li', 'ol', 'p', 'pre', 'strong', 'ul',
             ],
             attributes={
                 'a': ['href', 'title'],
+                'div': ['class'],
+                'h1': ['id'],
+                'h2': ['id'],
+                'h3': ['id'],
+                'h4': ['id'],
+                'h5': ['id'],
+                'h6': ['id'],
                 'img': ['alt', 'src', 'title'],
             },
         )
