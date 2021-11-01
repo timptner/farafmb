9ef4cec5b2afc11d075b618fcde16305cdc70f8e
Aiven <aiven@timptner.de>
2021-11-01 23:13:17 +0100

    Limit access to image upload form

--- about/views.py
+++ about/views.py
@@ -1,10 +1,12 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from .forms import ImageForm
 
 
+@login_required
 def upload_file(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
