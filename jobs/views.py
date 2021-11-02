0a1911002f61d1b06f1efe9feeb3c48105a6bdff
Aiven <aiven@timptner.de>
2021-11-02 15:35:13 +0100

    Add group filter to view

--- jobs/views.py
+++ jobs/views.py
@@ -7,5 +7,15 @@ from .models import Job
 
 class JobList(generic.ListView):
     model = Job
-    queryset = Job.objects.exclude(expired_on__lt=timezone.now())
     ordering = '-created_on'
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data['groups'] = Job.GROUP_CHOICES
+        return data
+
+    def get_queryset(self):
+        queryset = Job.objects.exclude(expired_on__lt=timezone.now())
+        if 'group' in self.request.GET:
+            queryset = queryset.filter(group=self.request.GET['group'])
+        return queryset
