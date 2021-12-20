44068e50d81c7f62964ab07edee551234b5b1ecc
Aiven <aiven@timptner.de>
2021-12-20 15:23:53 +0100

    Add permissions for authenticated users

--- farafmb/auth.py
+++ farafmb/auth.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
 
@@ -22,13 +23,50 @@ class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
             last_name=claims.get('family_name'),
             is_staff=True,
         )
+
+        # Check if superuser
+        if 'admin' in claims.get('roles', []):
+            user.is_admin = True
+
+        # Add user permissions via groups
+        self._add_groups(user, claims)
+
+        user.save()
+
         return user
 
     def update_user(self, user, claims):
         user.email = claims.get('email', '')
         user.first_name = claims.get('given_name', '')
         user.last_name = claims.get('family_name', '')
+
+        # Check if superuser
+        if 'admin' in claims.get('roles', []):
+            user.is_admin = True
+        else:
+            user.is_admin = False
+
+        # Remove user permissions via groups
+        for group in user.groups.all():
+            if group.name not in claims.get('roles', []):
+                user.groups.remove(group)
+
+        # Add user permissions via groups
+        self._add_groups(user, claims)
+
         user.save()
 
         return user
+
+    @staticmethod
+    def _add_groups(user, claims):
+        for role in claims.get('roles', []):
+            if role == 'admin':
+                continue
+            try:
+                group = Group.objects.get(name=role)
+            except Group.DoesNotExist:
+                group = Group.objects.create(name=role)
+
+            user.groups.add(group)
 
