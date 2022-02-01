609816efe4fb2d114188a38ce6dee88c75155ea5
Aiven Timptner <aiven@timptner.de>
2022-02-01 12:23:49 +0100

    Update dev settings

--- farafmb/settings_dev.py
+++ farafmb/settings_dev.py
@@ -2,13 +2,11 @@ from .settings import *
 
 DEBUG = True
 
+ALLOWED_HOSTS = []
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-]
