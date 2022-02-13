4b3571e632ebc4202881d80156fd49abc858b91e
Aiven Timptner <aiven@timptner.de>
2022-02-13 22:41:08 +0100

    Fix returning 301 on route without trailing slash

--- farafmb/urls.py
+++ farafmb/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
-from django.urls import include, path, reverse
+from django.urls import include, path, re_path, reverse
+from oauth2_provider.views import ConnectDiscoveryInfoView
 
 
 def redirect_admin(request):
@@ -24,6 +25,9 @@ urlpatterns = [
     path('jobs/', include('jobs.urls')),
     path('meetings/', include('meetings.urls')),
     path('members/', include('members.urls')),
+    re_path(r'^oauth/\.well-known/openid-configuration/?$',
+            ConnectDiscoveryInfoView.as_view(),
+            name='oidc-connect-discovery-info'),  # Serve discovery view on both routes
     path('oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('office-hours/', include('office_hours.urls')),
 ]
