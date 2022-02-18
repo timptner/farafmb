8c2dc7606cc9499e5851cd17e102b3dceb1dd03a
Aiven <aiven@timptner.de>
2022-02-18 14:01:09 +0100

    Avoid overwriting widget and set only custom help texts and labels

--- documents/forms.py
+++ documents/forms.py
@@ -7,12 +7,19 @@ from .models import Document
 
 
 class DocumentAdminForm(forms.ModelForm):
-    file = forms.FileField(help_text=_('The file name will be changed to a URL-compliant '
-                                       'version of the title after saving.'))
-
     class Meta:
         model = Document
         fields = ('title', 'file', 'visible')
+        labels = {
+            'title': _('Title'),
+            'file': _('File'),
+            'visible': _('Visible'),
+        }
+        help_texts = {
+            'file': _('The file name will be changed to a URL-compliant version of the title after saving.'),
+            'visible': _('This checkbox will only control if the file is listed on the documents view. The real '
+                         'URL to the file itself will still be accessible.'),
+        }
 
     def save(self, commit=True):
         form = super()
