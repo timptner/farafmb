6d286553d7227ff5ee1021e89bbb94b210b78d79
Aiven <aiven@timptner.de>
2022-02-21 10:44:31 +0100

    Remove link model, view, templates, etc from blog app

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostsView.as_view(), name='posts'),
     path('contact/', views.ContactView.as_view(), name='contact'),
-    path('links/', views.LinksView.as_view(), name='links'),
     path('protocols/', lambda request: redirect(reverse('exams:submit'), permanent=True)),
 ]
