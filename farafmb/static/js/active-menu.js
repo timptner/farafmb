807e47a1b614a7a5a631460ab640fbd4cd3f2290
Aiven Timptner <aiven@timptner.de>
2022-07-15 13:19:59 +0200

    Make admin navigation mobile ready

--- farafmb/static/js/active-menu.js
+++ farafmb/static/js/active-menu.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let menu = document.querySelector('#admin-nav');
+    let menu = document.querySelector('#adminNavigation');
 
     (menu.querySelectorAll('a') || []).forEach(($element) => {
         if ($element.href === document.URL) {
