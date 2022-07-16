b626344477e81a737bdfaa739ce8819f978be703
Aiven Timptner <aiven@timptner.de>
2022-07-16 11:44:59 +0200

    Update mentor form

--- mentoring/models.py
+++ mentoring/models.py
@@ -32,3 +32,23 @@ class Mentor(models.Model):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Helper(models.Model):
+    first_name = models.CharField(_("First name"), max_length=100)
+    last_name = models.CharField(_("Last name"), max_length=100)
+    email = models.EmailField(_("Email address"), unique=True)
+    phone = models.CharField(_("Mobile number"), max_length=25)
+    comment = models.TextField(_("Comment"), blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Helper")
+        verbose_name_plural = _("Helpers")
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
