c4f789bd793fc658c1243f4d9b4dba25119645d1
Aiven Timptner <aiven@timptner.de>
2022-07-16 11:45:29 +0200

    Update urls

--- mentoring/urls.py
+++ mentoring/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'mentoring'
 urlpatterns = [
-    path('program/all/', views.ProgramListView.as_view(), name='program-list'),
-    path('program/create/', views.ProgramCreateView.as_view(), name='program-create'),
+    path('program/', views.ProgramListView.as_view(), name='program-list'),
+    path('program/add/', views.ProgramCreateView.as_view(), name='program-create'),
     path('program/<int:pk>/delete/', views.ProgramDeleteView.as_view(), name='program-delete'),
-    path('mentor/all/', views.MentorListView.as_view(), name='mentor-list'),
-    path('mentor/create/', views.MentorCreateView.as_view(), name='mentor-create'),
-    path('mentor/create/done/', views.MentorCreateDoneView.as_view(), name='mentor-create-done'),
+    path('mentor/', views.MentorListView.as_view(), name='mentor-list'),
+    path('mentor/add/', views.MentorCreateView.as_view(), name='mentor-create'),
+    path('mentor/add/done/', views.MentorCreateDoneView.as_view(), name='mentor-create-done'),
     path('mentor/<int:pk>/', views.MentorDetailView.as_view(), name='mentor-detail'),
 ]
