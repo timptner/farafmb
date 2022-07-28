24560563c3e88a433bdb0a9c2168e6c164dd20b2
Aiven Timptner <aiven@timptner.de>
2022-07-29 01:16:56 +0200

    Add entry to menu and add temporary redirect for profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.shortcuts import redirect
 
 from . import views
 
@@ -9,6 +10,7 @@ urlpatterns = [
     path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
     path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('profile/', lambda request: redirect('members:profile_form'), name='profile'),
     path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
