2b123a2f3392ee9cdb74238ee5b610125e4a89c1
Aiven Timptner <aiven@timptner.de>
2022-07-29 01:04:54 +0200

    Use separate layout for auth views

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
 from . import forms
 
@@ -12,25 +15,28 @@ class LogoutView(auth_views.LogoutView):
     template_name = 'accounts/logout.html'
 
 
-class PasswordChangeView(auth_views.PasswordChangeView):
-    form_class = forms.PasswordChangeForm
-
-
-class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
-    pass
-
-
 class PasswordResetView(auth_views.PasswordResetView):
+    template_name = 'accounts/password_reset_form.html'
     form_class = forms.PasswordResetForm
+    email_template_name = 'accounts/password_reset_mail.html'
+    success_url = reverse_lazy('accounts:password_reset_done')
 
 
 class PasswordResetDoneView(auth_views.PasswordResetDoneView):
-    pass
+    template_name = 'accounts/password_reset_done.html'
 
 
 class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = 'accounts/password_reset_confirm.html'
     form_class = forms.SetPasswordForm
 
 
 class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
-    pass
+    template_name = 'accounts/password_reset_complete.html'
+
+
+class PasswordChangeView(SuccessMessageMixin, auth_views.PasswordChangeView):
+    template_name = 'accounts/password_change_form.html'
+    form_class = forms.PasswordChangeForm
+    success_url = reverse_lazy('accounts:password_change')
+    success_message = _("Your password has been changed successfully.")
