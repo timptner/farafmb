a15f67f9a3f26854079ebc7773c69bbfc7e2319c
Aiven Timptner <aiven@timptner.de>
2022-09-04 23:43:05 +0200

    Add tests to mentoring app

--- mentoring/tests.py
+++ mentoring/tests.py
@@ -6,6 +6,70 @@ from .forms import MentorForm
 from .models import Registration, Program, Mentor
 
 
+class PublicViewsTest(TestCase):
+    def setUp(self) -> None:
+        start = timezone.now()
+        stop = start+ timedelta(days=30)
+        self.registration = Registration.objects.create(name='Test', started_at=start, stopped_at=stop)
+        self.program = Program.objects.create(name='Test')
+        self.mentor = Mentor.objects.create(
+            registration=self.registration,
+            first_name='John',
+            last_name='Doe',
+            email='john.doe@example.org',
+            phone='+49123456789',
+            program=self.program,
+        )
+
+    def test_landing_view(self):
+        response = self.client.get('/mentoring/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mentor_list_view(self):
+        response = self.client.get('/mentoring/mentor/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_export_view(self):
+        response = self.client.get('/mentoring/mentor/export')
+        self.assertEqual(response.status_code, 301)
+
+    def test_mentor_create_view(self):
+        response = self.client.get('/mentoring/mentor/add/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mentor_create_done_view(self):
+        response = self.client.get('/mentoring/mentor/add/done/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mentor_detail_view(self):
+        response = self.client.get(f'/mentoring/mentor/{self.mentor.pk}/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_program_list_view(self):
+        response = self.client.get('/mentoring/program/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_program_create_view(self):
+        response = self.client.get('/mentoring/program/add/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_program_delete_view(self):
+        response = self.client.get(f'/mentoring/program/{self.program.pk}/delete/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_registration_list_view(self):
+        response = self.client.get('/mentoring/registration/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_registration_create_view(self):
+        response = self.client.get('/mentoring/registration/add/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_registration_delete_view(self):
+        response = self.client.get(f'/mentoring/registration/{self.registration.pk}/delete/')
+        self.assertEqual(response.status_code, 302)
+
+
 class ProgramTestCase(TestCase):
     def setUp(self) -> None:
         Program.objects.create(name='Engineering')
