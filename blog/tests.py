d0b8323b06562f1b8c2413b9c3526e91350fc764
Aiven Timptner <aiven@timptner.de>
2022-09-04 22:45:28 +0200

    Add tests to blog app

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class PublicViewsTest(TestCase):
+    def test_posts_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_view(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_exams_redirect(self):
+        response = self.client.get('/protocols/')
+        self.assertEqual(response.status_code, 301)
