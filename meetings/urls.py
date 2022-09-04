66ad37a63dd0dd8b7827041c05052c80284946f8
Aiven Timptner <aiven@timptner.de>
2022-09-04 23:28:51 +0200

    Disable meetings invite form

--- meetings/urls.py
+++ meetings/urls.py
@@ -4,6 +4,6 @@ from meetings import views
 
 app_name = 'meetings'
 urlpatterns = [
-    path('invite/', views.SendInvite.as_view(), name='invite'),
-    path('invite/success/', views.SendInviteSuccess.as_view(), name='invite-success'),
+    # path('invite/', views.SendInvite.as_view(), name='invite'),  # Todo Fix missing template before enabling again
+    # path('invite/success/', views.SendInviteSuccess.as_view(), name='invite-success'),
 ]
