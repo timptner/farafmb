db911234e5a4de8b7ef9fd1aae702ca216efb131
Aiven Timptner <aiven@timptner.de>
2022-09-06 08:59:37 +0200

    Add separate path for posts and limit the latest posts on landing page

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.PostsView.as_view(), name='posts'),
+    path('', views.LatestPostsView.as_view(), name='latest_posts'),
+    path('posts/', views.PostsView.as_view(), name='posts'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('protocols/', lambda request: redirect(reverse('exams:submit'), permanent=True)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,12 @@ class PostsView(ListView):
     template_name = 'blog/posts.html'
     model = Post
     ordering = ['-created']
+    paginate_by = 10
+
+
+class LatestPostsView(ListView):
+    template_name = 'blog/latest_posts.html'
+    queryset = Post.objects.order_by('-created').all()[:5]
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
