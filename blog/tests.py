bf52bf82fd0048805f660e908daa701ba43587cf
Aiven Timptner <aiven@timptner.de>
2022-09-14 23:52:39 +0200

    Update tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,15 +1,26 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
 
 class PublicViewsTest(TestCase):
-    def test_posts_view(self):
-        response = self.client.get('/')
+    def test_news_view(self):
+        response = self.client.get(reverse('blog:news'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_archive_view(self):
+        response = self.client.get(reverse('blog:archive'))
         self.assertEqual(response.status_code, 200)
 
     def test_contact_view(self):
-        response = self.client.get('/contact/')
+        response = self.client.get(reverse('blog:contact'))
         self.assertEqual(response.status_code, 200)
 
-    def test_exams_redirect(self):
-        response = self.client.get('/protocols/')
-        self.assertEqual(response.status_code, 301)
+
+class AdminViewsTest(TestCase):
+    def test_post_list_view(self):
+        response = self.client.get(reverse('blog:posts'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_create_view(self):
+        response = self.client.get(reverse('blog:create-post'))
+        self.assertEqual(response.status_code, 302)
