dd7755cab2a6a839c2c70d5823ac31f53742715f
Aiven Timptner <aiven@timptner.de>
2022-09-14 23:30:30 +0200

    Add form success url and message

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from datetime import date
-from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views import generic
 from meetings.models import Meeting
 
 from .forms import PostCreateForm
@@ -11,9 +14,17 @@ class PostListView(LoginRequiredMixin, generic.ListView):
     model = Post
 
 
-class PostCreateView(LoginRequiredMixin, generic.CreateView):
+class PostCreateView(LoginRequiredMixin, SuccessMessageMixin, generic.CreateView):
     model = Post
     form_class = PostCreateForm
+    success_message = _("%(title)s was created successfully")
+
+    def get_success_url(self):
+        return reverse('blog:posts')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class PostsView(generic.ListView):
