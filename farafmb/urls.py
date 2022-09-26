76995bfb2216a11efe0efd78184c80691f521812
Aiven Timptner <aiven@timptner.de>
2022-09-26 23:47:04 +0200

    Fix NoReverseMatch

--- farafmb/urls.py
+++ farafmb/urls.py
@@ -2,23 +2,12 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.shortcuts import redirect
-from django.urls import include, path, re_path, reverse
+from django.urls import include, path, re_path
 from oauth2_provider.views import ConnectDiscoveryInfoView
-
-
-def redirect_admin(request):
-    base_url = reverse('login')
-    if 'next' in request.GET:
-        url = f"{base_url}?next={request.GET.get('next')}"
-    else:
-        url = base_url
-    return redirect(url)
 
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
-    path('admin/login/', redirect_admin),
     path('admin/', admin.site.urls),
 
     path('i18n/', include('django.conf.urls.i18n')),
