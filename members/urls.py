49e5fb9733f845fe34d8e9a42bb9da827534bb05
Aiven Timptner <aiven@timptner.de>
2022-09-29 15:26:45 +0200

    Update member list view

--- members/urls.py
+++ members/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'members'
 urlpatterns = [
-    path('', views.MemberListView.as_view(), name='member_list'),
     path('update/', views.UserProfileFormView.as_view(), name='profile_form'),
+    path('', views.MemberListView.as_view(), name='member-list'),
     path('add/', views.MemberCreateView.as_view(), name='member-create'),
     path('profile/', views.get_profile, name='profile'),
 ]
