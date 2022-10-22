ea28a7ec3318d1d6f8aa9169063c3222dd9a853b
Aiven <aiven@timptner.de>
2022-10-22 10:11:49 +0200

    Rework office hours public view

--- consultations/urls.py
+++ consultations/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'consultations'
 urlpatterns = [
-    path('', views.ConsultationsView.as_view(), name='main'),
-    path('add/', views.ConsultationCreateView.as_view(), name='consultation-create'),
-    path('list/', views.ConsultationListView.as_view(), name='consultation-list'),
-    path('<int:pk>/update/', views.ConsultationUpdateView.as_view(), name='consultation-update'),
-    path('<int:pk>/delete/', views.ConsultationDeleteView.as_view(), name='consultation-delete'),
+    path('', views.OfficeHoursView.as_view(), name='office-hours'),
+    path('consultation/add/', views.ConsultationCreateView.as_view(), name='consultation-create'),
+    path('consultation/list/', views.ConsultationListView.as_view(), name='consultation-list'),
+    path('consultation/<int:pk>/update/', views.ConsultationUpdateView.as_view(), name='consultation-update'),
+    path('consultation/<int:pk>/delete/', views.ConsultationDeleteView.as_view(), name='consultation-delete'),
 ]
--- consultations/views.py
+++ consultations/views.py
@@ -1,10 +1,10 @@
+from datetime import time
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import ConsultationForm
 from .models import Consultation
-from .utils import calc_max_step_size, time_to_seconds, seconds_to_time
 
 
 class ConsultationCreateView(LoginRequiredMixin, generic.CreateView):
@@ -40,31 +40,24 @@ class ConsultationDeleteView(LoginRequiredMixin, generic.DeleteView):
     success_url = reverse_lazy('consultations:consultation-list')
 
 
-class ConsultationsView(generic.TemplateView):
-    template_name = 'consultations/main.html'
+class OfficeHoursView(generic.TemplateView):
+    template_name = 'consultations/office_hours.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        context['data'] = {}
-        office_hours = Consultation.objects.filter(is_visible=True).all()
-        if office_hours:
-            time_list = [office_hour.time for office_hour in office_hours]
-            seconds = calc_max_step_size(time_list)
-            if seconds == 0:
-                seconds = 7200
-            points = range(
-                time_to_seconds(min(time_list)),
-                time_to_seconds(max(time_list)) + seconds + seconds,
-                seconds,
-            )
-            for time_ in [seconds_to_time(seconds) for seconds in points]:
-                slots = []
-                for day, name in Consultation.DAYS:
-                    if time_ in time_list:
-                        slots.append(office_hours.filter(time=time_, day=day).all())
-                    else:
-                        slots.append([])
-                context['data'][time_] = slots
+        earliest = min(Consultation.objects.values_list('start', flat=True))
+        latest = max(Consultation.objects.values_list('end', flat=True))
+        context['earliest'] = earliest
+        context['latest'] = latest
+        data = {
+            'hours': [time(hour=hour) for hour in range(earliest.hour, latest.hour + 1)],
+        }
+        for day, display in Consultation.DAYS:
+            data[day] = []
+            for hour in range(earliest.hour, latest.hour + 1):
+                data[day].append(
+                    Consultation.objects.filter(day=day, start__hour=hour).all()
+                )
+        context['data'] = map(list, zip(*data.values()))  # transpose matrix
         context['days'] = Consultation.DAYS
         return context
