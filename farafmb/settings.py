99a46ea9dffa7ec18684fbb66dedbe443027df11
Aiven Timptner <aiven@timptner.de>
2023-02-11 21:09:13 +0100

    Fix allowed hosts setting

--- farafmb/settings.py
+++ farafmb/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
