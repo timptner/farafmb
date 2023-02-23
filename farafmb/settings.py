8c088043c8ca42bddbe8a144ab883de68df621bd
Aiven Timptner <aiven@timptner.de>
2023-02-23 21:52:34 +0100

    Disable site tracking for local development

--- farafmb/settings.py
+++ farafmb/settings.py
@@ -12,6 +12,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
+INTERNAL_IPS = ['127.0.0.1']
+
 
 # Application definition
 
