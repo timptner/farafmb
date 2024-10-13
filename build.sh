2d358e85968599ae57342a2ab68d83d0e43b5422
Aiven Timptner <aiven@timptner.de>
2024-10-13 19:44:23 +0200

    update build workflow

--- build.sh
+++ build.sh
@@ -1,51 +1,69 @@
 #!/usr/bin/env bash
 
-# create base image
-container=$(buildah from debian)
+log_step () {
+    echo -e "\n>>>>> $1 <<<<<\n"
+}
 
-# install dependencies
-buildah run $container -- apt-get update
-buildah run $container -- apt-get --yes install \
-    python3 python3-dev python3-venv            \
-    libpq-dev build-essential
+{
+    log_step "pull debian image as base"
+    container=$(buildah from debian)
+    echo "new container: $container"
 
-# create app user
-name=farafmb
-app_dir=/opt/$name
-buildah run $container -- useradd   \
-    --shell /bin/bash               \
-    --home-dir $app_dir $name
-buildah config --user $name --workingdir $app_dir $container
+    log_step "install dependencies"
+    buildah run $container -- apt-get update
+    buildah run $container -- apt-get --yes install \
+        python3 python3-dev python3-venv            \
+        libpq-dev build-essential
 
-# copy project
-buildah copy --user $name $container . $app_dir
+    log_step "create application user"
+    name=farafmb
+    app_dir=/opt/$name
+    buildah run $container -- useradd   \
+        --shell /bin/bash               \
+        --create-home                   \
+        --home-dir $app_dir             \
+        $name
 
-# install python depdencies
-buildah run --user $name $container -- python -m venv .venv
-buildah run --user $name $container -- .venv/bin/pip install -r ./requirements.txt
-buildah run --user $name $container -- .venv/bin/pip install gunicorn
+    log_step "copy project files"
+    buildah copy $container . $app_dir
+    buildah run $container -- chown -R $name:$name $app_dir
 
-# create public dir
-public_dir=/srv/$name
-buildah run $container -- mkdir $public_dir
-buildah run $container -- chown -R $name:$name $public_dir
-buildah config --volume $public_dir $container
+    log_step "create service directories"
+    public_dir=/srv/$name
+    buildah run $container -- mkdir $public_dir
+    buildah run $container -- chown -R $name:$name $public_dir
+    buildah config --volume $public_dir $container
 
-# collect static files
-buildah config                              \
-    --env STATIC_ROOT=$public_dir/static    \
-    --env MEDIA_ROOT=$public_dir/media      \
-    $container
-buildah run --user $name $container -- .venv/bin/python manage.py collectstatic --no-input
+    log_step "set default user and working directory"
+    buildah config              \
+        --user $name            \
+        --workingdir $app_dir   \
+        $container
 
-# configure container
-port=8000
-run_cmd="$app_dir/.venv/bin/gunicorn --workers 2 --bind 0.0.0.0:$port farafmb.wsgi"
-buildah config                              \
-    --cmd $run_cmd                          \
-    --port $port                            \
-    --env LOG_FILE=$public_dir/server.log   \
-    $container
+    log_step "install python packages"
+    buildah run $container -- python3 -m venv .venv
+    buildah run $container -- .venv/bin/pip install -r ./requirements.txt
+    buildah run $container -- .venv/bin/pip install gunicorn
 
-# commit container as new image
-buildah commit $container "farafmb"
+    log_step "collect static files"
+    buildah config                              \
+        --env STATIC_ROOT=$public_dir/static    \
+        --env MEDIA_ROOT=$public_dir/media      \
+        $container
+    buildah run $container -- .venv/bin/python manage.py collectstatic --no-input
+
+    log_step "configure server"
+    port=8000
+    run_cmd="$app_dir/.venv/bin/gunicorn --workers 2 --bind 0.0.0.0:$port farafmb.wsgi"
+    buildah config                              \
+        --cmd "$run_cmd"                        \
+        --port $port                            \
+        --env LOG_FILE=$app_dir/farafmb.log     \
+        $container
+
+    log_step "commit container as new image"
+    image=$(buildah commit --rm $container "farafmb")
+
+} >&2
+
+echo $image
